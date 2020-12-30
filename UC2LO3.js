2513919e6c9dff897e3169afcceb5b121aebc8e5
JCDayauon <demonlordex700@gmail.com>
2020-12-30 17:32:43 +0900

    first commit

---
+++ UC2LO3.js
@@ -0,0 +1,39 @@
+// for(i = 0; i < 10; i++){
+// 	console.log(i);
+// 	if(i === 9){
+// 	 break;
+// 	}
+// }
+
+// console.log ("end of loop");
+
+
+// var links = document.getElementsByTagName("a");
+
+// for (i = 0; i < links.length; i++){
+// links[i].className = "link-" + i ;
+// }
+
+// function getAverage (a,b){
+// 	var average = (a + b)/ 2;
+// 	console.log (average);
+// 	return average;
+// }
+
+// var Result = getAverage(21,13);
+// console.log ("Average is = " + Result);
+
+var a = 15;
+var b = 13;
+
+if (isNaN(a)){
+	console.log ("Not a number");
+
+}
+else {
+	console.log("Use a calculator nxt time anyways here's your answer " + (a * b ));
+}
+
+
+
+
